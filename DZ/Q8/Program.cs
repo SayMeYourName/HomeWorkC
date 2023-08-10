ca8f8a7da3e25fe4231bc75b7a863996b75473b9
Ion <jus4sky@gmail.com>
2023-08-10 23:00:26 +0300

    Решение 8й задачи

---
+++ DZ/Q8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.)");
+Console.WriteLine("Enter N number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+int N1 = N % 2;
+Console.WriteLine(N1);
+while (i < N)
+{
+    if (N1 <= 1)
+    {
+        i = i + 1;
+    }
+    System.Console.Write($"{i} ");
+    i++;
+}
+// System.Console.WriteLine();
