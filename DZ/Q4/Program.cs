1f394857163fac99770a51b88b0c39b3c07a46dd
Ion <jus4sky@gmail.com>
2023-08-10 22:30:52 +0300

    Решение 4й задачи

---
+++ DZ/Q4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine("Enter a number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b number");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter c number");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (a < b)
+{
+    max = b;
+}   
+    if (c > max)
+    {
+        max = c;
+    }   
+ 
+System.Console.Write($"{max} наибольшее число");
+// Условия не подставлял, решил сделать универсальный вариант для любых чисел.
