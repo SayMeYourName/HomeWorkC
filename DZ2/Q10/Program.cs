aa62d37af795b594741b5bb110e45816f7bc5d24
Ion <jus4sky@gmail.com>
2023-08-13 01:20:52 +0300

    Решение 10й задачи

---
+++ DZ2/Q10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter XXX number");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100 && num > 1000)
+System.Console.WriteLine("Вы ввели не трехзначное число");
+int digit1 = num / 10 % 10;
+System.Console.WriteLine($"Second number {digit1}");
