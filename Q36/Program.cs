747c7d985f09a889225f727e2fa20bcfa9984742
Yaroslav <141037304+SayMeYourName@users.noreply.github.com>
2023-09-09 00:52:21 +0300

    Add files via upload

---
+++ Q36/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void Main(string[] args)
+{
+    Console.Write("Введите длину массива: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[length];
+    // Заполняем массив случайными трёхзначными числами
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-1000, 1000);
+        // Выводим сгенерированный массив
+    }
+    Console.WriteLine("Массив: ");
+    Console.WriteLine();
+    foreach (int element in array)
+    {
+        Console.Write("{0} ", element);
+    }
+    // Находим сумму элементов на нечетных позициях
+    int sum = 0;
+        for (int i = 0; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    // Выводим результат
+    Console.WriteLine();
+    Console.WriteLine($"Сумма нечетных элементов : {sum}");
+}
+Main(args);
---
+++ Q38/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+}
+void PrintArray(double[] array)
+{
+    foreach (double item in array)
+        System.Console.Write($"{item:F2} ");
+    System.Console.WriteLine();
+}
+double GetMax(double[] array)   
+    {    
+        double max = array[0];  
+        foreach (double value in array)  
+            if (value > max)  
+                max = value;  
+        
+        System.Console.Write($"Максимальное значение числа в массиве: {max:F2}");
+    System.Console.WriteLine();
+    return max;
+    }    
+double GetMin(double[] array)    
+    {      
+        double min = array[0];          
+        foreach (double value in array)      
+            if (value < min)        
+                min = value;      
+        
+        System.Console.Write($"Минимальное значение числа в массиве: {min:F2}");
+    System.Console.WriteLine(); 
+    return min; 
+    }      
+System.Console.WriteLine("Введите количество элементов массива");
+int len = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[len];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine($"Разница равна: {GetMax(array)- GetMin(array)}");
